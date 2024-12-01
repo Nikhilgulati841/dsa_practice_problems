bf365f0509eeaa10ff78c9b72618468e3a958946
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-12-01 21:51:18 +0000

    alternate option fucntion

--- DSA Course/analysis of Algorithms/sum_of_natural.py
+++ DSA Course/analysis of Algorithms/sum_of_natural.py
@@ -2,19 +2,21 @@ from datetime import datetime
 
 a=datetime.now()
 
-# def natural_sum(num):
-#     total=0
-#     for i in range(1,num+1):
-#         # total=total+i
-#         total+=i
-
-#     return total
-
 def natural_sum(num):
     total=0
     for i in range(1,num+1):
-        for j in range(1,i+1):
-            total=total+1
+        # total=total+i
+        total+=i
+
+    return total
+
+# def natural_sum(num):  #without actually summing the actual natural numbers
+#     total=0
+#     for i in range(1,num+1):
+#         for j in range(1,i+1):
+#             total=total+1
+
+#     return total
 
 print(natural_sum(11))
 
