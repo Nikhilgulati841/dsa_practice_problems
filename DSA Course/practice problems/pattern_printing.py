e851594111322f977d85060f03e1172e77fbe15b
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-12-08 21:00:22 +0000

    added star|number|alphabets different patterns

--- DSA Course/practice problems/pattern_printing.py
+++ DSA Course/practice problems/pattern_printing.py
@@ -61,3 +61,59 @@ for i in range(1,n+1):
         print(" "*(n-i)+f"{j} "*i)
 
 
+# -----------------------------------
+
+
+# print()
+# for i in range(1,n+1):
+#     if i==6:
+#         print("*"*(i+1)+" "*(n-i-1)+" "*(n-i-1)+"*"*(i+1))
+        
+#     else:
+#         print("*"*i+" "*(n-i)+" "*(n-i)+"*"*i)
+    
+    
+# for i in range(n-1,0,-1):
+#     if i==6:
+#         print("*"*(i+1)+" "*(n-i-1)+" "*(n-i-1)+"*"*(i+1))
+#     else:
+#         print("*"*i+" "*(n-i)+" "*(n-i)+"*"*i)
+        
+# print()
+# for i in range(1,n+1):
+#     print("*"*i+" "*(n-i)+" "+" "*(n-i)+"*"*i)
+    
+# for i in range(n-1,0,-1):
+#     print("*"*i+" "*(n-i)+" "+" "*(n-i)+"*"*i)
+print()
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(1,i+1):
+        print(f"{j} ",end="")
+        
+    print()
+    
+print()
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print(f"{chr(65+j)} ",end="")
+        
+    print()
+# print(chr(65)) -- A
+    
+    
+
+print()
+for i in range(1,n+1):
+    print(" "*(n-i)+f"{i} "*i)
+
+print()
+
+for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+print()
+
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*i)
+  
